e00e1a44900f7b6f07676a60ca5f2cf43b91f952
git-satya108 <145919632+git-satya108@users.noreply.github.com>
2024-07-29 01:52:29 +0530

    Update data-genai.py

--- data-genai.py
+++ data-genai.py
@@ -6,7 +6,7 @@ import pandas as pd
 import langchain
 import langchain_openai
 from langchain_community.document_loaders import TextLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
